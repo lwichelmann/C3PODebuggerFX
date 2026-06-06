c1e80e342dee374f097d634e5a8af7e61bbb1d68
Leon Wichelmann <leon.wichelmann@web.de>
2026-06-06 16:23:04 +0200

    enhanced handle of missed spelled files controller or view

--- src/main/java/de/soulfoxer/c3podebugger/ViewLoader.java
+++ src/main/java/de/soulfoxer/c3podebugger/ViewLoader.java
@@ -3,14 +3,36 @@ package de.soulfoxer.c3podebugger;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import java.io.IOException;
+import java.net.URL;
 
 public final class ViewLoader {
 
-    public static Parent load(Class<?> controllerClass) throws IOException {
-        String name = controllerClass.getSimpleName().replace("Controller", "View");
-        String fxml = name + ".fxml";
+    private ViewLoader() {
+    }
 
-        FXMLLoader loader = new FXMLLoader(controllerClass.getResource(fxml));
-        return loader.load();
+    public static Parent load(Class<?> controllerClass) throws IOException {
+        String controllerName = controllerClass.getSimpleName();
+
+        if (!controllerName.endsWith("Controller")) {
+            throw new IOException("Could not load view: Controller class name must end with 'Controller': " + controllerName);
+        }
+
+        String viewName = controllerName.replaceFirst("Controller$", "View");
+        String fxml = viewName + ".fxml";
+
+        URL fxmlUrl = controllerClass.getResource(fxml);
+
+        if (fxmlUrl == null) {
+            throw new IOException("Could not load view: " + fxml);
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxmlUrl);
+
+        try {
+            return loader.load();
+        }catch(IOException e) {
+            throw new IOException("Could not load view: " + fxml, e);
+        }
+
     }
 }
